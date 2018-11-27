fac39892537d4b1330171ef398da0b29aac4b121
protpolymer <nvsvkont@gmail.com>
2018-11-27 17:52:38 +0300

    add return in condition

--- src/main/java/ru/itpark/BonusService.java
+++ src/main/java/ru/itpark/BonusService.java
@@ -2,36 +2,32 @@ package ru.itpark;
 
 public class BonusService {
     public int calculateBonus(int purchaseAmount, int totalAmount) {
-        int result = 0;
 
-        int minBlueValue = 1;
         int maxBlueValue = 15_000;
         int bonusBlue = 50;
 
-        int minSilverValue = 15_001;
         int maxSilverValue = 150_000;
         int bonusSilver = 70;
 
         int bonusGold = 100;
 
         if (totalAmount == 0) {
-            result = 0;
+            return 0;
 
-        } else if (totalAmount >= minBlueValue && totalAmount <= maxBlueValue) {
+        } else if (totalAmount <= maxBlueValue) {
             // blue card
 
-            result = (int)purchaseAmount / 1000 * bonusBlue;
+            return purchaseAmount / 1000 * bonusBlue;
 
-        } else if (totalAmount >= minSilverValue && totalAmount <= maxSilverValue) {
+        } else if (totalAmount <= maxSilverValue) {
             // silver card
 
-            result = (int)purchaseAmount / 1000 * bonusSilver;
+            return purchaseAmount / 1000 * bonusSilver;
 
         } else {
             // gold card
 
-            result = (int)purchaseAmount / 1000 * bonusGold;
+            return purchaseAmount / 1000 * bonusGold;
         }
-        return result;
     }
 }
